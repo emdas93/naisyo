d5969e00248aa74da67c0813a82fd8ad5f281cb2
emdas93 <emdas93@gmail.com>
2024-11-24 17:44:30 +0900

    TEST 데이터 통신 완료

--- api/main.py
+++ api/main.py
@@ -19,7 +19,29 @@ def test():
         "id": 1,
         "name": "Test Data",
         "description": "This is a simple test response",
-        "status": "active"
+        "status": "active",
+        "message": """
+아래는 요청하신 쿼리문 입니다. (Flask)
+
+```sql
+SELECT no, name, room, room_id, heat, no, testcol, opsco, posco
+FROM table_name AS Table
+LEFT JOIN join_table_name AS JOIN_TABLE
+ON TABLE.no = JOIN_TABLE.table_no
+WHERE TABLE.id=1
+```
+
+query의 결과는 아래 표와 같습니다.
+
+|no|name|room|room_id|heat|no|testcol|opsco|posco|
+|---|---|---|---|---|---|---|---|---|
+|데이터1|데이터2|데이터3|데이터1|데이터2|데이터3|데이터1|데이터2|데이터3|
+|데이터4|데이터5|데이터6|데이터4|데이터5|데이터6|데이터4|데이터5|데이터6|
+|데이터7|데이터8|데이터9|데이터7|데이터8|데이터9|데이터7|데이터8|데이터9|
+|데이터1|데이터2|데이터3|데이터1|데이터2|데이터3|데이터1|데이터2|데이터3|
+|데이터4|데이터5|데이터6|데이터4|데이터5|데이터6|데이터4|데이터5|데이터6|
+|데이터7|데이터8|데이터9|데이터7|데이터8|데이터9|데이터7|데이터8|데이터9|
+"""
     })
 
 if __name__ == '__main__':
